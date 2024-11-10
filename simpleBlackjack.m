454e8433f4d4f06abeae0acb64487810cabfa58a
motomaselli18 <morgan.tomaselli@slu.edu>
2024-11-10 17:44:42 -0600

    Update simpleBlackjack.m

--- simpleBlackjack.m
+++ simpleBlackjack.m
@@ -13,3 +13,20 @@ function avgScore = simpleBlackjack(limit, trials)
 % OUTPUT avgScore – The average final score of
 % all hands.
 
+
+
+function avgScore = simpleBlackjack(limit, trials)
+    finalScores = zeros(1, trials);
+    cardValues = [2:10 10 10 10];
+    
+    for trial = 1:trials
+        handTotal = 0;
+        while handTotal < limit
+            card = cardValues(randi(length(cardValues)));
+            handTotal = handTotal + card;
+        end
+        finalScores(trial) = handTotal;
+    end
+    
+    avgScore = mean(finalScores);
+end
