a49c1debf157d930b246a80b373e99ff07056fda
motomaselli18 <morgan.tomaselli@slu.edu>
2024-11-10 15:26:09 -0600

    Update fruit.m

--- fruit.m
+++ fruit.m
@@ -5,3 +5,19 @@ function [] = fruit( N )
 % The number itself if neither is true
 % “applebanana” if both are true
 
+for number = 1:N
+        
+        if rem(number, 2) == 0 && rem(number, 5) == 0
+            fprintf('applebanana\n');
+    
+        elseif rem(number, 2) == 0
+            fprintf('apple\n');
+ 
+        elseif rem(number, 5) == 0
+            fprintf('banana\n');
+     
+        else
+            fprintf('%d\n', number);
+        end
+    end
+end
